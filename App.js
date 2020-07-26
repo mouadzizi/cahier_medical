c2e9e39df95f790d4b2b7fed6a16328c7ecb6a5f
mouad.zizi <mouadzizi@users.noreply.github.com>
2020-07-26 01:35:42 +0100

    commit 3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-
+import {TouchableOpacity, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
@@ -9,10 +9,21 @@ import SignUp from './src/pages/SignUp';
 import Home from './src/pages/Home';
 import Profil from './src/pages/Profil';
 
-import {auth, db} from './src/data/Firebase';
+import {auth} from './src/data/Firebase';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const StackNav = createStackNavigator();
+const Container = createStackNavigator();
 const TabNav = createMaterialTopTabNavigator();
+
+
+{/* sign out function with firebase*/}
+function signOutUser() {
+        
+  auth.signOut()
+  .catch(err=>Alert.alert('Error',err.message))
+}
+
 
 function Authentication(){
 return(
@@ -48,6 +59,43 @@ function MyTabs() {
   );
 }
 
+function MyContainer(){
+  return(
+    <Container.Navigator initialRouteName="MyTabs">
+
+    <Container.Screen name="MyTabs" component={MyTabs}
+
+    options={{ title: 'Carnet Medical',
+               headerTitleAlign: 'center',
+               headerStyle: {
+               backgroundColor: '#3498DB'},
+               headerTintColor: '#fff',
+               headerTitleStyle:{
+               fontWeight: 'bold',
+               },
+               headerLeft: () => (
+                <TouchableOpacity
+                style={{marginLeft: 30}}
+                onPress={()=>signOutUser()}>
+                <FontAwesome5
+                name='power-off'
+                size={30}
+                color='#fff'
+                />
+                </TouchableOpacity> ),
+              headerRight:()=>(
+                  <Text 
+                    style={{color:'white',fontWeight:'bold',fontSize:18}}
+                  > @{auth.currentUser.displayName} </Text>
+                ),
+              headerRightContainerStyle:{
+                  paddingRight:8
+                }
+    }}/>
+    </Container.Navigator>
+  );
+}
+
 export default function App() {
   const [user, setUser] = useState(false);
 
@@ -68,7 +116,7 @@ export default function App() {
       <NavigationContainer>
     {
       !user?
-      <Authentication /> : <MyTabs /> 
+      <Authentication /> : <MyContainer /> 
     }
 
       </NavigationContainer>
